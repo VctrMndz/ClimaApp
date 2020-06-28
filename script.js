0cc0e1b9fa154273f70a233c53555bc7ba5f7d7d
Vctr <vctrmndz77@gmail.com>
2020-06-28 01:38:06 -0600

    mobile view

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const iconElement = document.querySelector('.icon-container');
 const tempElement = document.querySelector('.temp-value');
 const descElement = document.querySelector('.description');
 const windElement = document.querySelector('.wind-value');
-const rainElement = document.querySelector('.rain.value');
+const cloudsElement = document.querySelector('.clouds-value');
 
 const weather = {};
 
 // GET WEATHER FROM API PROVIDER
 
 function getWeather() {
-	let api = `https://api.openweathermap.org/data/2.5/onecall?lat=13.717637&lon=-89.230232&appid=d952b628a09f946583811f297e3af8fd&units=metric`;
+	let api = `https://api.openweathermap.org/data/2.5/onecall?lat=13.717637&lon=-89.230232&appid=d952b628a09f946583811f297e3af8fd&units=metric&exclude=minutely,hourly,daily&lang=sp`;
 
 	fetch(api)
 		.then(function(response) {
@@ -21,7 +21,7 @@ function getWeather() {
 			weather.description = data.current.weather[0].description;
 			weather.iconId = data.current.weather[0].icon;
 			weather.wind = data.current.wind_speed;
-			weather.rain = data.current.rain;
+			weather.clouds = data.current.clouds;
 		})
 		.then(function() {
 			displayWeather();
@@ -30,11 +30,11 @@ function getWeather() {
 
 // DISPLAY WEATHER TO UI
 function displayWeather() {
-	iconElement.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather.iconId}@2x.png"/>`;
-	tempElement.innerHTML = `${weather.temperature}°C`;
-	windElement.innerHTML = `${weather.wind} m/s`;
-	rainElement.innerHTML = weather.rain;
-	descElement.innerHTML = weather.description;
+	iconElement.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather.iconId}@4x.png"/>`;
+	tempElement.innerHTML = ` ${weather.temperature}° C`;
+	windElement.innerHTML = `  ${weather.wind} m/s`;
+	cloudsElement.innerHTML = ` ${weather.clouds}%`;
+	descElement.innerHTML = weather.description[0].toUpperCase() + weather.description.substring(1);
 }
 
 getWeather();
